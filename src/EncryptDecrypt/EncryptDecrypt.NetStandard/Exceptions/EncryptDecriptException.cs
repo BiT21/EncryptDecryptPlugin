a236eee9b41481c902c8a6ada46e3cce449f178b
raulg.bit21 <raulg.bit21@outlook.com>
2018-02-17 12:53:11 +0100

    Building .\src\EncryptDecrypt\EncryptDecrypt.NetStandard"

---
+++ src/EncryptDecrypt/EncryptDecrypt.NetStandard/Exceptions/EncryptDecriptException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncryptDecrypt.NetStandard.Exceptions
+{
+    public class EncryptDecriptException : Exception
+    {
+        public EncryptDecriptException(string message) : base(message)
+        {
+        }
+        public EncryptDecriptException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+
+    public class EncryptDecryptExceptionDataCorruption : Exception
+    {
+        public EncryptDecryptExceptionDataCorruption(string message) : base(message)
+        {
+        }
+        public EncryptDecryptExceptionDataCorruption(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ src/EncryptDecrypt/EncryptDecrypt.NetStandard/IEncryptDecrypt.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+
+namespace EncryptDecrypt.NetStandard
+{
+    public interface IEncryptDecrypt
+    {
+        /// <summary>
+        /// Decrypts data with the given format.
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="data"></param>
+        /// <returns>Decripted string.</returns>
+        /// <exception cref="EncryptDecryptExceptionDataCorruption">Thrown when is detected that the data provided has been modified since the encryption.</exception>        
+        /// <exception cref="EncryptDecriptException">General error decrypting. It could be wrong password.</exception>
+        Task<string> DecryptStringAsync(string password, string data);
+
+        /// <summary>
+        /// Encrypts data with the given password.
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="data"></param>
+        /// <returns>Return the encrypted data on Base64 format</returns>
+        /// <exception cref="EncryptDecriptException">General error exception.</exception>
+        Task<string> EncryptStringAsync(string password, string data);
+    }
+}
